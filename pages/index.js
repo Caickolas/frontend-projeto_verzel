4a4546a4c1776b0feabaaa8b0a18b017ca79c538
Caick da Silva Galvão <88976384+Caickolas@users.noreply.github.com>
2022-05-24 13:11:50 -0300

    login e cadastro

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Botao from '../componentes/botao'
+import Login from './login'
+
 
 export default function Home() {
   return (
-    <>
-    <h1>Olá Mundo</h1>
-    <div> 
-     <Botao texto={'Login'} cor='primaria' onClick={() => console.log('botão clicado!')}></Botao>
-    </div>
-    </>
+    <Login />
   )
 }
---
+++ pages/login/index.js
@@ -0,0 +1,87 @@
+import InputPublico from "../../componentes/inputPublico";
+import Image from "next/image";
+import Botao from "../../componentes/botao";
+import Link from "next/link";
+import { useState } from "react";
+import {validarEmail, validarSenha } from '../../utils/validadores'
+//import UsuarioService from "../../services/UsuarioService";
+
+import imagemEnvelope from "../../public/imagens/envelope.svg"
+import imagemChave from "../../public/imagens/chave.svg"
+
+//const usuarioService = new UsuarioService();
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [estaSubmetendo, setEstaSubmetendo] = useState(false);
+
+    const validarFormulario = () => {
+        return (
+            validarEmail(email) && validarSenha(senha)
+        );
+    };
+
+    const aoSubmeter = async (e) => {
+        e.preventDefault();
+        if (!validarFormulario()) {
+            return;
+        }
+
+        setEstaSubmetendo(true);
+
+        try {
+
+            await usuarioService.login({
+                login: email,
+                senha
+            });
+
+        } catch (error) {
+            alert(
+                "erro ao logar usuario." + error?.response?.data?.erro
+            );
+        }
+
+        setEstaSubmetendo(false);
+    }
+
+    return (
+        <section className={'paginaLogin paginaPublica'}>            
+            <div className="conteudoPaginaPublica">
+                <form onSubmit={aoSubmeter}>
+                    <InputPublico
+                        imagem = {imagemEnvelope}
+                        texto = {"E-mail"}
+                        type="email"
+                        aoAlterarValor={e => setEmail(e.target.value)}
+                        valor={email}
+                        mensagemValidacao="O endereço informado é invalido"
+                        exibirMensagemValidacao={email && !validarEmail(email)}
+                    />
+
+                    <InputPublico
+                        imagem={imagemChave}
+                        texto={"Senha"}
+                        type="password"
+                        aoAlterarValor={e => setSenha(e.target.value)}
+                        valor={senha}
+                        mensagemValidacao="Precisa ter ao menos 4 caracteres"
+                        exibirMensagemValidacao={senha && !validarSenha(senha)}
+                    />
+
+                    <Botao 
+                    texto="Login"
+                    tipo="submit"
+                        desabilitado={!validarFormulario() || estaSubmetendo }
+                    />
+                </form>
+
+                <div className="rodapePaginaPublica">
+                    <p>Não possui uma Conta?</p>
+                    <Link href="/cadastro"> Faça seu cadastro agora!</Link>
+                </div>
+            </div>
+        </section>
+    )
+}
